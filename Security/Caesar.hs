e664d470ef7a8a7caf1962cfd5f03f50cecad5ed
Jim Stanton <Jim@jhstanton.com>
2013-10-14 03:15:41 -0500

    Added Caesar cypher

---
+++ Security/Caesar.hs
@@ -0,0 +1,35 @@
+{-
+  Caesar.hs
+  
+  Implements a basic caesar cypher. 
+-}
+import Data.List
+
+main = 
+  do 
+  putStr "Enter any alphabetical string to encrypt: "
+  str <- getLine
+  putStr "Enter the positive integer key <= 26: "
+  keyStr <- getLine
+  let key = read keyStr::Int
+  let strs = words str
+  let encryptedWords = map (\x ->encrypt x key) strs
+  let decryptedWords = map (\x ->decrypt x key) encryptedWords
+  putStr "Encrypted string: "
+  putStrLn $ intercalate " " encryptedWords
+  putStr "Decrypted back: "
+  putStrLn $ intercalate " " decryptedWords
+  
+-- wraps infinitely
+lower = ['a' .. 'z'] ++ lower
+upper = ['A' .. 'Z'] ++ upper
+
+shift word amt = 
+  map (\x -> if elem x (take 26 lower)
+             then head $ drop amt $ dropWhile (/= x) lower
+             else head $ drop amt $ dropWhile (/= x) upper) word 
+
+encrypt word key = shift word key 
+
+decrypt word key = shift word (26 - key)
+  
